5450215809ae6066a260900cc3670103e51747b7
raamizz <rameezebrahim@transpost.co>
2025-01-10 16:52:08 +0530

    Updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'sisl_pw_app',
-  webDir: 'build',
-  plugins:{
-    "camera":{
-      "saveToGallery":true
-    }
-  }
+  webDir: 'build'
 };
 
 export default config;
